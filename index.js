cd30bde3cb802c96a96fb2e3c5b0ea68659edfdf
Elena Ryzhkova <grishina.elena.p@yandex.ru>
2023-05-19 22:56:59 +0300

    попап и доверстала

---
+++ index.js
@@ -0,0 +1,45 @@
+const openPopupButtonEl = document.querySelector('.profile__open-popup');
+const closePopupButtonEl = document.querySelector('#close-popup-button');
+const editPopupEl = document.querySelector('#edit-popup');
+const pageTitleEl = document.querySelector('.profile__name');
+const pageProfessionEl = document.querySelector('.profile__profession');
+const nameInputEl = document.querySelector('#name-input');
+const professionInputEl = document.querySelector('#profession-input');
+const editFormEl = document.querySelector('#edit-form');
+
+openPopupButtonEl.addEventListener('click', function () {
+  openPopup(editPopupEl);
+});
+
+closePopupButtonEl.addEventListener('click', function () {
+  closePopup(editPopupEl);
+});
+
+nameInputEl.value = pageTitleEl.textContent;
+
+editFormEl.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  pageTitleEl.textContent = nameInputEl.value;
+
+  closePopup(editPopupEl);
+});
+
+professionInputEl.value = pageProfessionEl.textContent;
+
+editFormEl.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  pageProfessionEl.textContent = professionInputEl.value;
+
+  closePopup(editPopupEl);
+});
+
+
+function openPopup(popupEl) {
+  popupEl.classList.add('popup_is-opened');
+}
+
+function closePopup(popupEl) {
+  popupEl.classList.remove('popup_is-opened');
+}
