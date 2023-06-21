90c4f4b6f5bec2c9dd5b241d5f059103b3b4cc55
Elena Ryzhkova <grishina.elena.p@yandex.ru>
2023-06-21 17:47:30 +0300

    доработки

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,6 @@
-const buttonOpenEditProfilePopup = document.querySelector(".profile__open-popup");
+const buttonOpenEditProfilePopup = document.querySelector(
+  ".profile__open-popup"
+);
 const buttonOpenAddCardPopup = document.querySelector(".profile__add");
 
 const buttonCloseEditProfilePopup = document.querySelector("#close-edit-form");
@@ -143,3 +145,104 @@ function openPopup(popupEl) {
 function closePopup(popupEl) {
   popupEl.classList.remove("popup_opened");
 }
+
+const formElement = document.querySelector(".popup__form");
+const formInput = formElement.querySelector(".popup__input");
+const formError = formElement.querySelector(`.${formInput.id}-error`);
+
+//добавление класса и показ ошибки
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add('popup__input_type-error');
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('popup__input-error_active');
+};
+
+//убирание класса и убирание ошибки
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove('popup__input_type-error');
+  errorElement.classList.remove('popup__input-error_active');
+  errorElement.textContent = '';
+};
+
+const checkInputValidity = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+
+const toggleButtonState = (inputList, buttonElement) => {
+  if (hasInvalidInput(inputList)) {
+    // сделай кнопку неактивной
+    buttonElement.classList.add('popup__button_disabled');
+  } else {
+    // иначе сделай кнопку активной
+    buttonElement.classList.remove('popup__button_disabled');
+  }
+};
+//масштабируемость
+function enableValidation() {
+  
+  const formList = Array.from(document.querySelectorAll(".popup__form"));
+  formList.forEach((formElement) => {
+    formElement.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+    });
+    setEventListeners(formElement);
+  });
+}
+
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  })
+  
+};
+
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+  const buttonElement = formElement.querySelector('.popup__button');
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', function () {
+      checkInputValidity(formElement, inputElement);
+      toggleButtonState(inputList, buttonElement);
+    });
+  });
+};
+
+
+enableValidation({
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__button',
+  inactiveButtonClass: 'popup__button_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__error_visible'
+}); 
+
+
+//закрытие попапов
+const popups = document.querySelectorAll(".popup");
+//закрытие через escape
+popups.forEach(function (popup) {
+window.addEventListener('keydown', function(event) {
+  if (event.keyCode === 27) {
+      closePopup(popup);
+  }
+
+ //закрытие через overlay
+
+  
+});
+popup.addEventListener("click", (evt) => {
+  if (evt.currentTarget === evt.target) {
+    closePopup(popup)
+  };
+});
+
+});
+
+
